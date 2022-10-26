eff9fdc0e11fbd49e175e8ee959e686852654217
u31510 <u31510@P31510>
2022-10-26 12:11:15 +1000

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Rassokhin_PR_5_YP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void z_odin(object sender, RoutedEventArgs e)
+        {
+            odin.NavigationService.Navigate(new Uri("Page1.xaml", UriKind.Relative));
+        }
+
+        private void z_dva(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+
+        private void z_tri(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+
+        private void z_hetire(object sender, RoutedEventArgs e)
+        {
+            Window3 window3 = new Window3();
+            window3.Show();
+        }
+
+        private void z_payti(object sender, RoutedEventArgs e)
+        {
+            Window4 window4 = new Window4();
+            window4.Show();
+
+        }
+    }
+}
---
+++ Page8.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Rassokhin_PR_5_YP
+{
+    /// <summary>
+    /// Логика взаимодействия для Page8.xaml
+    /// </summary>
+    public partial class Page8 : Page
+    {
+        public Page8()
+        {
+            InitializeComponent();
+        }
+        public class Phone
+        {
+            public string Title { get; set; }
+            public string Company { get; set; }
+            public int Price { get; set; }
+        }
+        List<Phone> phonesList = new List<Phone>
+{
+    new Phone { Title="iPhone 6S", Company="Apple", Price=54990 },
+    new Phone {Title="Lumia 950", Company="Microsoft", Price=39990 },
+    new Phone {Title="Nexus 5X", Company="Google", Price=29990 }
+};
+
+
+
+    }
+}
